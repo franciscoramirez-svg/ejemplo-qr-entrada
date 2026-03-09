b0038d2794fa3a9bc990358c06a4fc98a0e04ab3
franciscoramirez-svg <francisco.ramirez@neomotic.com>
2026-03-09 12:47:51 -0600

    Update ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -9,20 +9,7 @@ from math import radians, cos, sin, asin, sqrt
 import pytz
 from datetime import datetime
 
-zona_veracruz = pytz.timezone('America/Mexico_City')
-if data:
-    if st.button(f"Confirmar Registro para {data}"):
-        # 2. Obtener hora exacta de Veracruz
-        ahora_veracruz = datetime.now(zona_veracruz)
-        hora_formateada = ahora_veracruz.strftime("%d/%m/%Y %H:%M:%S")
 
-        # 3. Guardar con la hora corregida
-        nuevo = pd.DataFrame([[data, hora_formateada, lat_actual, lon_actual]], 
-                             columns=["Empleado", "Hora", "Lat", "Lon"])
-        
-        nuevo.to_csv(DB_FILE, mode='a', header=not os.path.exists(DB_FILE), index=False)
-        st.success(f"Registrado a las {hora_formateada}")
-        st.balloons()
 # --- CONFIGURACIÓN DE LA OFICINA ---
 OFICINA_LAT = 19.245304  
 OFICINA_LON = -96.174232 
@@ -95,4 +82,5 @@ if os.path.exists(DB_FILE):
         st.info("Aún no hay registros el día de hoy.")
 else:
     st.info("El archivo de base de datos se creará con el primer registro.")
+
 
