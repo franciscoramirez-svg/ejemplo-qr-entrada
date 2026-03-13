24825fcba6aa46897914dfccb7a4a2041af7fa43
franciscoramirez-svg <francisco.ramirez@neomotic.com>
2026-03-13 11:32:37 -0600

    Update ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -107,7 +107,7 @@ def enviar_reporte_semanal(df):
         
         csv_name = f"REPORTE_ASISTENCIA_{hoy.strftime('%d_%m_%Y')}.csv"
         part = MIMEBase('application', 'octet-stream')
-        part.set_payload(csv_final.to_csv(index=False).encode('utf-8'))
+        part.set_payload(csv_final.to_csv(index=False).encode('utf-8')).encode('utf-8-sig'))
         encoders.encode_base64(part)
         part.add_header('Content-Disposition', f'attachment; filename="{csv_name}"')
         msg.attach(part)
