d6d0f051ed2029383e0935c069c855906a3f902a
franciscoramirez-svg <francisco.ramirez@neomotic.com>
2026-03-13 13:32:09 -0600

    Update ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -12,6 +12,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+import urllib.parse
 
 # --- 1. CONFIGURACIÓN INICIAL ---
 zona_veracruz = pytz.timezone('America/Mexico_City')
