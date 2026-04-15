33be188682818ebaf30fc78dadcd3beb09653dc1
franciscoramirez-svg <francisco.ramirez@neomotic.com>
2026-04-15 15:25:44 -0600

    Create supabase_client.py

---
+++ services/supabase_client.py
@@ -0,0 +1,8 @@
+from supabase import create_client
+import streamlit as st
+
+@st.cache_resource
+def get_supabase():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
